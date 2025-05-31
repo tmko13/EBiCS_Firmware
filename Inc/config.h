51e3037c8a38afdced9210e32b98ebd21dfe2935
tmko13 <97597352+tmko13@users.noreply.github.com>
2025-05-31 17:34:11 +0200

    mit actualiserter config datei config.h

--- Inc/config.h
+++ Inc/config.h
@@ -8,17 +8,22 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 #include "stdint.h"
-#define DISPLAY_TYPE_EBiCS (1<<5)  
-#define DISPLAY_TYPE_KINGMETER_618U (1<<4)                  // King-Meter 618U protocol (KM5s, EBS-LCD2, J-LCD, SW-LCD)
-#define DISPLAY_TYPE_KINGMETER_901U (1<<8)                  // King-Meter 901U protocol (KM5s)
-#define DISPLAY_TYPE_KINGMETER      (DISPLAY_TYPE_KINGMETER_618U|DISPLAY_TYPE_KINGMETER_901U)
-#define DISPLAY_TYPE_BAFANG (1<<2)							// For 'Blaupunkt' Display of Prophete Entdecker
-#define DISPLAY_TYPE_KUNTENG (1<<1)							// For ASCII-Output in Debug mode
+
+// System constants, don't touch!
 #define DISPLAY_TYPE_DEBUG (1<<0)							// For ASCII-Output in Debug mode);
+#define DISPLAY_TYPE_KUNTENG (1<<1)							// For Kunteng display
+#define DISPLAY_TYPE_BAFANG_LCD (1<<2)						// For 'Blaupunkt' Display of Prophete Entdecker
+#define DISPLAY_TYPE_BAFANG_850_860 (1<<3)					// Bafang 850/860. Can do 9k6 baud, also compatible with 1200 baud detection at startup
+#define DISPLAY_TYPE_KINGMETER_618U (1<<4)                  // King-Meter 618U protocol ( J-LCD)
+#define DISPLAY_TYPE_KINGMETER_901U (1<<5)                  // King-Meter 901U protocol (KM5s)
+#define DISPLAY_TYPE_EBiCS (1<<6)                  			// Protocol using the ANT+ LEV logic
+#define DISPLAY_TYPE_NO2 (1<<7)								// For China Protokoll "No_2" S866 display for example
+#define DISPLAY_TYPE_BAFANG         (DISPLAY_TYPE_BAFANG_LCD|DISPLAY_TYPE_BAFANG_850_860)
+#define DISPLAY_TYPE_KINGMETER      (DISPLAY_TYPE_KINGMETER_618U|DISPLAY_TYPE_KINGMETER_901U)
 #define EXTERNAL 1
 #define INTERNAL 0
-#define LEGALFLAG 
 
+//----------------------------------------------------------------------
 #define TRIGGER_OFFSET_ADC 50
 #define TRIGGER_DEFAULT 2020
 #define _T 2028
@@ -66,7 +71,7 @@
 #define VOLTAGE_MAX 1600
 #define SPEC_ANGLE -715827882
 #define TS_MODE
-#define DISPLAY_TYPE DISPLAY_TYPE_BAFANG //Bafang Displays, including 'Blaupunkt' 
+#define DISPLAY_TYPE DISPLAY_TYPE_BAFANG_LCD //Bafang Displays, including 'Blaupunkt' 
 #define SPEED_PLL 0
 #define SPEEDSOURCE EXTERNAL
 #define AUTODETECT 0
